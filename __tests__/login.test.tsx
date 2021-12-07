ffe007a90a16d838a564c8d423213a2f6750900d
Glen Byrne <glenobyrne123@gmail.com>
2021-12-07 17:49:56 +0000

    Update jest.config.js to support module path aliases

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Login from '@/pages/login';
+import { ReactElement } from 'react';
+import { MockedProvider as ApolloMockProvider } from '@apollo/client/testing';
+
+const renderWithApollo = (element: ReactElement) => {
+  render(element, { wrapper: ApolloMockProvider });
+};
 
 describe('Login', () => {
-  render(<Login />);
+  it('Renders', () => {
+    renderWithApollo(<Login />);
+  });
 
   const heading = screen.getByText('Sign in to your account');
 
--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,17 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@/constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@/core/(.*)$': '<rootDir>/src/core/$1',
+    '^@/utilities/(.*)$': '<rootDir>/src/utilities/$1',
+    '^@/generated/(.*)$': '<rootDir>/src/generated/$1',
+    '^@/styles/(.*)$': '<rootDir>/src/styles/$1',
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
